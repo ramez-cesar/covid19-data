cdff9605e53752778c8600e660e1b342fc1fc006
César Ramírez <cesarramez8@gmail.com>
2022-06-23 17:59:36 -0500

    Adding new format date and section new cases in [main.js] file in the section /src/js

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,13 @@ const dateUpdated = document.querySelector('#date_updated')
 const globalCases = document.querySelector('#global_cases')
 const globalDeaths = document.querySelector('#global_deaths')
 const globalRecovered = document.querySelector('#global_recovered')
+const globalNewCases = document.querySelector('#global_new_cases')
 
 
 const URL_API_GLOBAL = 'https://covid-19.dataflowkit.com/v1/world';
+
+
+const months = ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic',]
 
 
 async function globalData() {
@@ -13,11 +17,17 @@ async function globalData() {
 
     if(response.ok) {
 
-        dateUpdated.textContent = data['Last Update']
-        console.log(data['Total Cases_text'])
+        const date = new Date([data['Last Update']])
+
+        const day = date.getDate()
+        const month = date.getMonth()
+        const year = date.getFullYear() 
+
+        dateUpdated.textContent = `${day}, ${months[month]}, ${year}`
         globalCases.textContent = data['Total Cases_text']
         globalDeaths.textContent = data['Total Deaths_text']
         globalRecovered.textContent = data['Total Recovered_text']
+        globalNewCases.textContent = data['New Cases_text']
 
     } else {
         showError.textContent = `Hubo un error! ${response.status} ${data.message}`
