e7a391585995ce880c733eff14756c78073f22ee
Cesar <cesarramez8@gmail.com>
2023-06-27 11:43:39 -0600

    Add node file

---
+++ src/js/nodes.js
@@ -0,0 +1,42 @@
+// Node of global data
+const mainGlobalData = document.querySelector('.main--global-data')
+const messageError = document.querySelector('#message_error')
+const dateUpdated = document.querySelector('#date_updated')
+const globalCases = document.querySelector('#global_cases')
+const globalDeaths = document.querySelector('#global_deaths')
+const globalRecovered = document.querySelector('#global_recovered')
+const globalNewCases = document.querySelector('#global_new_cases')
+
+// Nodes of search
+const searchCountry = document.querySelector('#search_country')
+const mainContainerCountry = document.querySelector('.main--container-country')
+const btnRandomCountry = document.querySelector('#btn_random_country')
+
+// Node of firts table
+const countryNameOne = document.querySelector('#country_name_one')
+const newCasesOne = document.querySelector('#new_cases_one')
+const newActiveCasesOne = document.querySelector('#new_active_cases_one')
+const newDeathsOne = document.querySelector('#new_deaths_one')
+const totalCasesOne = document.querySelector('#total_cases_one')
+const totalDeseacedOne = document.querySelector('#total_deaths_one')
+const totalRecoveredOne = document.querySelector('#total_recovered_one')
+
+// Node of second table
+const countryNameTwo = document.querySelector('#country_name_two')
+const newCasesTwo = document.querySelector('#new_cases_two')
+const newActiveCasesTwo = document.querySelector('#new_active_cases_two')
+const newDeathsTwo = document.querySelector('#new_deaths_two')
+const totalCasesTwo = document.querySelector('#total_cases_two')
+const totalDeseacedTwo = document.querySelector('#total_deaths_two')
+const totalRecoveredTwo = document.querySelector('#total_recovered_two')
+
+// Node of three table
+const countryNameThree = document.querySelector('#country_name_three')
+const newCasesThree = document.querySelector('#new_cases_three')
+const newActiveCasesThree = document.querySelector('#new_active_cases_three')
+const newDeathsThree = document.querySelector('#new_deaths_three')
+const totalCasesThree = document.querySelector('#total_cases_three')
+const totalDeseacedThree = document.querySelector('#total_deaths_three')
+const totalRecoveredThree = document.querySelector('#total_recovered_three')
+
+const copyrigth = document.querySelector('.copyrigth')
